bd17a84321f23bb07e4860320ec3ac75b76b59f9
HuangJyunKai <58419378+HuangJyunKai@users.noreply.github.com>
2020-02-01 15:29:51 +0800

    Add files via upload

--- unet_model.py
+++ unet_model.py
@@ -8,7 +8,7 @@ from unet_parts import *
 
 
 class Unet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=True):
+    def __init__(self, n_channels, n_classes, bilinear=False):
         super(Unet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
@@ -19,14 +19,15 @@ class Unet(nn.Module):
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
         self.down4 = Down(512, 512)
-        self.up1 = Up(1024, 256, bilinear)
-        self.up2 = Up(512, 128, bilinear)
-        self.up3 = Up(256, 64, bilinear)
-        self.up4 = Up(128, 64, bilinear)
+        self.up1 = Up(1024, 256) #self.up1 = Up(1024, 256, bilinear)
+        self.up2 = Up(512, 128)
+        self.up3 = Up(256, 64)
+        self.up4 = Up(128, 64)
         self.outc = OutConv(64, n_classes)
+        #self.outm = OutConv(192, n_classes)
 
-    def forward(self, x):
-        x1 = self.inc(x)
+    def forward(self, x0):
+        x1 = self.inc(x0)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
@@ -35,6 +36,28 @@ class Unet(nn.Module):
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
+        '''
+        y1 = self.inc(x0)
+        y2 = self.down1(y1)
+        y3 = self.down2(y2)
+        y4 = self.down3(y3)
+        y5 = self.down4(y4)
+        y = self.up1(y5, y4)
+        y = self.up2(y, y3)
+        y = self.up3(y, y2)
+        y = self.up4(y, y1)
+        
+        z1 = self.inc(x0)
+        z2 = self.down1(z1)
+        z3 = self.down2(z2)
+        z4 = self.down3(z3)
+        z5 = self.down4(z4)
+        z = self.up1(z5, z4)
+        z = self.up2(z, z3)
+        z = self.up3(z, z2)
+        z = self.up4(z, z1)
+         '''
+        #out = torch.cat([x,y,z],dim=1)
+        #logits = self.outm(out)
         logits = self.outc(x)
-        #logits = nn.Sigmoid()(logits)
         return logits
